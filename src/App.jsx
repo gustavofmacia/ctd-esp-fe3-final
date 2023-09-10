f7bda77195e03f9f2c5dcaa00418ce7f5e478351
gustavofmacia <gustavofmacia@gmail.com>
2023-09-10 16:35:23 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Footer from "./Components/Footer";
+import Navbar from "./Components/Navbar";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Footer />
+    </div>
+  );
+}
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "../assets/images/DH.png";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Powered by</p>
+      <img src={logo} alt="Logo de Digital House" />
+    </footer>
+  );
+};
+
+export default Footer;
