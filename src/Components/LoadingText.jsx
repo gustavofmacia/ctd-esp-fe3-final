fb44009fce72901a538e10b668810a1df8b2c631
gustavofmacia <gustavofmacia@gmail.com>
2023-09-15 23:52:58 -0300

    cambio menor

--- src/Components/LoadingText.jsx
+++ src/Components/LoadingText.jsx
@@ -1,9 +1,9 @@
 export default function LoadingText() {
   return (
     <div>
-      <p style={{ fontSize: "2rem", margin: "10vh 0 40vh 20vh" }}>
+      <h2 style={{ fontSize: "2rem", margin: "10vh 0 40vh 20vh" }}>
         Cargando...
-      </p>
+      </h2>
     </div>
   );
 }
